f84726d50571b6fd0d9676615537f5c0795476fd
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-14 18:06:36 +0100

    Update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_comments_counter
+
   def update_comments_counter
     post.update(CommentsCounter: post.comments.count)
   end
