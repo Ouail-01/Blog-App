34d6104dd755f0409d33817cce025892272ab9f7
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-19 00:19:50 +0100

    Add posts tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /posts" do
-    it "works! (now write some real specs)" do
-      get posts_index_path
-      expect(response).to have_http_status(200)
-    end
+describe 'GET all posts for a user' do
+  let(:user) { User.create!(Name: 'Test User', PostsCounter: 0) }
+
+  it 'returns a successful response' do
+    get "/users/#{user.id}/posts"
+
+    expect(response).to be_successful
+    expect(response.body).to include('<h1>Posts for a given user</h1>')
+    expect(response).to render_template(:index)
+  end
+end
+
+describe 'GET specific post for a user' do
+  let(:user) { User.create!(Name: 'Test User', PostsCounter: 0) }
+
+  it 'renders a specific post from a user' do
+    post = Post.create!(Title: 'Test Post', author_id: user.id, CommentsCounter: 0, LikesCounter: 0)
+    get "/users/#{user.id}/posts/#{post.id}"
+
+    expect(response).to be_successful
+    expect(response.body).to include('<h1>Posts</h1>')
+    expect(response).to render_template(:show)
   end
 end
