4a331c1ee99e90edfb6f9645e1a396ee7b284ed6
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-18 18:16:00 +0100

    Fix the linters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     @user = User.includes(:posts).find(params[:id])
     @posts = @user.posts
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
