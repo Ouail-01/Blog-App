2c394875fda43569d7e5b25d03abfa2e1b7de602
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-26 22:18:02 +0100

    Devise setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  ​
 
   protected
 
-  ​
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
