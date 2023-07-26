97e4484722602198f4b7d745399326dc22a243a9
Adholah96 <adholah96@gmail.com>
2023-07-26 11:36:55 +0300

    Tests in post index and ruby

---
+++ spec/feature/post_index_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'Render post index page', type: :feature do
+  before :each do
+    @user = User.create(Name: 'Adhola', Photo: 'https://www.linkedtour.com/images/lkavatar.png',
+                        Bio: 'I am Micronaut, studying full stack development', PostsCounter: 0)
+    @first_post = Post.create(Title: 'Microverse',
+                              Text: 'Microverse online teaching school for those who want to be developers',
+                              CommentsCounter: 0, LikesCounter: 10, author: @user)
+    @second_post = Post.create(Title: 'Microverse', Text: 'The same post like the first one', CommentsCounter: 0,
+                               LikesCounter: 5,
+                               author: @user)
+    5.times do |_i|
+      Comment.create(Text: 'Nice post!!', author_id: @user.id, post_id: @first_post.id)
+    end
+    visit "/users/#{@user.id}/posts"
+  end
+
+  scenario 'displays post text' do
+    expect(page).to have_content('Microverse online teaching school for those who want to be developers')
+  end
+
+  scenario 'display the post title' do
+    expect(page).to have_content(@first_post.Title)
+  end
+
+  scenario 'display the post body' do
+    expect(page).to have_content(@first_post.Text)
+  end
+
+  scenario 'display the first comment on a post' do
+    expect(page).to have_content('Microverse')
+  end
+
+  scenario 'display the how many comments' do
+    expect(page).to have_content('Comments: 0')
+  end
+
+  scenario 'display the how many Likes' do
+    expect(page).to have_content('Likes: 5')
+  end
+
+  scenario "when click on a post, it redirects to the post's show page" do
+    expect(page).to have_content(@first_post.Title)
+  end
+end
