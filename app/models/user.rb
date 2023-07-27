f7440ef05262af894f3bc614cbf394ce4a39af6d
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-27 14:52:45 +0100

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 
+  before_validation -> { self.Name = email.split('@')[0] }
+
+  def set_avatar
+    max_id = User.maximum('id')
+    new_id = max_id ? max_id + 1 : 1
+    self.photo ||= "https://i.pravatar.cc/100?u=#{new_id}"
+  end
+
   def latest_three_posts
     posts.includes(:author).order(created_at: :desc).limit(3)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :sessions
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
