42f34ffbbec64a349157f886b80245d886af214a
Adholah96 <adholah96@gmail.com>
2023-07-28 20:33:55 +0300

    Updae post and user models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
-
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
+​
+  before_save -> { User.find_by(id: author.id).increment!(:posts_counter) }
+  before_destroy -> { User.find_by(id: author.id).decrement!(:posts_counter) }
+​
+  scope :get_5_comments, ->(post) { post.comments.order('created_at DESC').limit(5) }
+​
   validates :title, presence: true
-  validates :title, length: { maximum: 250 }
-  validates :comment_counter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { allow_blank: true, greater_than_or_equal_to: 0 }
-
-  def update_post_counter
-    author.update(posts_counter: author.posts.count)
-  end
-
-  def latest_five_comments
-    comments.includes(:post).order(created_at: :desc).limit(5)
-  end
+  validates :author, presence: true
+  validates :title, length: { in: 1..250 }
+  validates :likes_counter, :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
