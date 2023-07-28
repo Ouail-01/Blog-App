625b426a711bf3379768cf3603894792aa52c553
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-28 18:16:25 +0100

    Update the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email bio photo password current_password])
-  end
 end
