3fe070a4206924322b6ecf2261b21d1ae16b2457
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-28 18:15:20 +0100

    Add role to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root 'users#index'
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :new, :show, :create, :destroy] do
+      resources :comments, only: [:new, :index, :create, :destroy]
       resources :likes, only: [:new, :create]
     end
   end
-  
+
   post '/users/:id/posts/create', to: 'posts#create'
 end
