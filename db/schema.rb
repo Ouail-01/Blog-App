0eaa2fe2c9d0c1524f3691682829b227aebfd2f5
Ouail-01 <ouaillaarabi@gmail.com>
2023-07-28 20:03:37 +0100

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_160506) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.text "bio"
+    t.string "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
